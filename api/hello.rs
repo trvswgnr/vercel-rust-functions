3a8f0e44060e078bfd4f6229e30c5080abe3a990
Travis Wagner <travis.wagner@umusic.com>
2023-04-07 15:16:49 -0400

    update

--- api/hello.rs
+++ api/hello.rs
@@ -9,11 +9,11 @@ async fn main() -> Result<(), Error> {
 pub async fn handler(_req: Request) -> Result<Response<Body>, Error> {
     let body = reqwest::get("https://jsonplaceholder.typicode.com/todos/1")
         .await?
-        .json()
+        .text()
         .await?;
 
     Ok(Response::builder()
         .status(StatusCode::OK)
         .header("Content-Type", "application/json")
-        .body(json!(body).to_string().into())?)
+        .body(json!({ "message": body }).to_string().into())?)
 }
